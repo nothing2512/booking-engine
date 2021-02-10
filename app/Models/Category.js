3bd89fedf566f04202e284530b3bfb6fe675a3e2
nothing2512 <blank345red@gmail.com>
2021-02-10 19:18:03 +0700

    get consultation list

--- app/Models/Category.js
+++ app/Models/Category.js
@@ -3,6 +3,9 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
+/** @type {typeof import('../Helpers/Engine')} */
+const Engine = use('App/Helpers/Engine');
+
 /**
  * Category Model
  *
@@ -10,6 +13,15 @@ const Model = use('Model');
  * @extends Model
  */
 class Category extends Model {
+
+    /**
+     * get table name
+     *
+     * @return {string}
+     */
+    static get table() {
+        return `${Engine.lower("book")}_categories`
+    }
 }
 
 module.exports = Category;
