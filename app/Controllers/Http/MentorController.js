3af023db0eb3a56e9bbe730cb57ee508db4c90e1
nothing2512 <blank345red@gmail.com>
2021-02-17 18:15:02 +0700

    mentor controller v0.5.0

--- app/Controllers/Http/MentorController.js
+++ app/Controllers/Http/MentorController.js
@@ -1,161 +1,270 @@
 'use strict';
 
-/** @type {import('@adonisjs/lucid/src/Database')} */
-const Database = use('Database');
+const
+    /**@type {typeof import('../../Models/MentorSchedule')} */
+    Schedule = use('App/Models/MentorSchedule'),
 
-/** @type {typeof import('../../Models/Admin')} */
-const Admin = use('App/Models/Admin');
+    /**@type {typeof import('../../Models/Consultation')} */
+    Consultation = use('App/Models/Consultation'),
 
-/** @type {typeof import('../../Models/User')} */
-const User = use('App/Models/User');
+    /**@type {typeof import('../../Models/User')} */
+    User = use('App/Models/User'),
 
-/** @type {typeof import('../../Models/Role')} */
-const Role = use('App/Models/Role');
+    /**@type {typeof import('../../Helpers/Engine')} */
+    Engine = use('App/Helpers/Engine'),
 
-/** @type {typeof import('../../Models/AggregatorProfile')} */
-const AggregatorProfile = use('App/Models/AggregatorProfile');
-
-/** @type {typeof import('../../Models/UserAttachment')} */
-const UserAttachment = use('App/Models/UserAttachment');
-
-/** @type {typeof import('../../Models/UserProfile')} */
-const UserProfile = use('App/Models/UserProfile');
-
-/** @type {typeof import('../../Models/Category')} */
-const Category = use('App/Models/Category');
-
-/** @type {typeof import('../../Models/AggregatorMentor')} */
-const AggregatorMentor = use('App/Models/AggregatorMentor');
-
-/** @type {typeof import('../../Models/MentorSpecialization')} */
-const Specialization = use('App/Models/MentorSpecialization');
-
-/** @type {typeof import('../../Helpers/Engine')} */
-const Engine = use('App/Helpers/Engine');
+    /**@type {typeof import('../../Models/AggregatorMentor')} */
+    AggregatorMentor = use('App/Models/AggregatorMentor');
 
 /**
- * Mentor Controller
+ * Mentor Schedule Controller
  *
- * @class MentorController
+ * @class MentorScheduleController
  */
-class MentorController {
+class MentorScheduleController {
 
     /**
-     * get Mentor detail
+     * create mentor schedule
      *
-     * @method mentor_detail
+     * @method store
      * @async
      *
-     * @param mentor
-     * @returns {Promise<*>}
+     * @param request
+     * @param response
+     * @returns {Promise<void|*>}
      */
-    async mentor_detail(mentor) {
-        delete mentor.fcm;
-        delete mentor.is_accepted;
-        let subquery = Database.from(`${Engine.lower("aggregator")}_${Engine.lower("mentor")}s`)
-            .where(Engine.id("mentor"), mentor.id)
-            .select(Engine.id("aggregator"));
-        mentor.aggregatorProfile = await AggregatorProfile.findBy('user_id', subquery);
-        mentor.attachment = await UserAttachment.findBy('user_id', mentor.id);
-        mentor.specialization = await Category.query()
-            .whereIn("id", Database.from(`${Engine.lower("mentor")}_specializations`).where(Engine.id("mentor"), mentor.id).select("category_id"))
+    async store({request, response}) {
+        const day = request.input("day")
+        const mentor_id = request.get(Engine.id("mentor"));
+
+        const mentor = await User.find(mentor_id);
+        if (mentor == null) return response.notFound("Reader");
+
+        const schedules = await Schedule.query()
+            .where(Engine.id("mentor"), '=', mentor_id)
+            .where('day', '=', day)
             .fetch();
 
-        return mentor
+        if (schedules.toJSON().length > 0) {
+
+            /**
+             * If Exists, Update It
+             */
+            const params = {...schedules[0], ...request.all()};
+            await Schedule.query()
+                .where(Engine.id("mentor"), '=', mentor_id)
+                .where('day', '=', day)
+                .update(params);
+
+            return response.success(params)
+        } else {
+
+            /**
+             * If Doesn't Exists, Create It
+             */
+            const schedule = await Schedule.create(request.all());
+            return response.success(schedule)
+        }
     }
 
     /**
-     * get most active Mentor
-     * by aggregator
+     * get mentor time available by given date
      *
-     * @method mostActive
+     * @method timeAvailable
      * @async
      *
-     * @param auth
-     * @param response
-     * @returns {Promise<void|*>}
-     */
-    async mostActive({auth, response}) {
-        const user = await auth.getUser();
-        if (user.role_id != 3) return response.forbidden();
-
-        const subcount = `(SELECT COUNT(*) FROM consultations WHERE ${Engine.id("mentor")} = u.id AND status = 2)`;
-        let query = `SELECT u.*, ${subcount} as total_consultation `;
-        query += "FROM users u";
-        query += ` WHERE u.id IN (SELECT ${Engine.id("mentor")} FROM ${Engine.lower("aggregator")}_${Engine.lower("mentor")}s WHERE ${Engine.id("aggregator")} = ?)`;
-        query += "ORDER BY total_consultation DESC";
-        const result = [];
-        const mentors = await Database.raw(query, [user.id]);
-
-        for (let mentor of mentors.rows) result.push(await this.mentor_detail(mentor));
-
-        return response.success(result)
-    }
-
-    /**
-     * show Mentor by username
-     *
-     * @method showByUsername
-     * @async
-     *
-     * @param params
-     * @param response
-     * @returns {Promise<void|*>}
-     */
-    async showByUsername({params, response}) {
-        let user = await User.findBy('username', params.username);
-
-        if (user == null || user.role_id != 2) return response.notFound(Engine.title("mentor"));
-
-        user.role = await Role.find(user.role_id);
-        user.profile = await UserProfile.findBy('user_id', user.id);
-        if (user.role_id == 2) user = await this.mentor_detail(user);
-
-        return response.success(user)
-    }
-
-    /**
-     * edit mentor specialization
-     *
-     * @method editSpecialization
-     * @async
-     *
-     * @param auth
      * @param params
      * @param request
      * @param response
      * @returns {Promise<void|*>}
      */
-    async editSpecialization({auth, params, request, response}) {
-        let user = await auth.getUser();
-        let isAccessible = user instanceof Admin;
+    async timeAvailable({params, request, response}) {
+        const date = request.input("date");
+        const day = new Date(date).getDay() + 1;
+        const available = [];
 
-        if (user instanceof User) {
-            if (user.role_id == 3 && params.id != user.id) isAccessible = (await AggregatorMentor.query()
-                .where(Engine.id("aggregator"), user.id)
-                .where(Engine.id("mentor"), params.id)
-                .getCount()) > 0;
-            else isAccessible = params.id == user.id
+        const mentor = await User.find(params[Engine.id("mentor")]);
+        if (mentor == null) return response.notFound(Engine.title("mentor"));
+
+        const schedule = await Schedule.query()
+            .where(Engine.id("mentor"), params[Engine.id("mentor")])
+            .where('day', day)
+            .first()
+
+        if (schedule == null) return response.notFound("Schedule")
+
+        const consultations = (await Consultation.query()
+            .where(Engine.id("mentor"), params[Engine.id("mentor")])
+            .where('date', date)
+            .where('approval_status', '<>', 1)
+            .fetch()).toJSON();
+
+        const start_time = parseInt(schedule.start_time.split(":")[0]);
+        const end_time = parseInt(schedule.end_time.split(":")[0]);
+
+        for (let i = start_time; i <= end_time - 1; i++) {
+            let time = i < 10 ? `0${i}:00:00` : `${i}:00:00`;
+            available.push({
+                time: time,
+                is_available: !consultations.some(x => x.time == time)
+            })
         }
 
-        if (!isAccessible) return response.forbidden();
+        return response.success(available)
+    }
 
-        const categoryIds = request.input('category_id', []);
-        await Specialization.query()
-            .where(Engine.id("mentor"), params.id)
-            .delete();
+    /**
+     * get mentor available dates
+     *
+     * @method dateAvailable
+     * @async
+     *
+     * @param params
+     * @param response
+     * @returns {Promise<void|*>}
+     */
+    async dateAvailable({params, response}) {
+        const now = new Date();
+        const available = [];
+        const mentor = await User.find(params[Engine.id("mentor")]);
+        if (mentor == null) return response.notFound(Engine.title("mentor"));
 
-        const payloads = [];
-        for (let categoryId of categoryIds) {
-            if (await Category.find(id) == null) return response.notFound("Category");
-            let payload = {};
-            payloads[Engine.id("mentor")] = params.id;
-            payloads.category_id = categoryId;
-            payloads.push(payload)
+        if (parseInt(mentor.role_id) !== 2) return response.notFound(`User is not ${Engine.lower("mentor")}`);
+
+        for (let i = 0; i <= 30; i++) {
+            let month = now.getMonth() + 1;
+            let dates = now.getDate();
+            let date = `${now.getFullYear()}-${month <= 9 ? "0" : ""}${month}-${dates <= 9 ? "0" : ""}${dates}`;
+            let day = now.getDay() + 1;
+            now.setDate(now.getDate() + 1);
+
+            let schedule = (await Schedule.query()
+                .where(Engine.id("mentor"), mentor.id)
+                .where('day', day)
+                .fetch()).toJSON()[0];
+
+            let start_time = parseInt(schedule.start_time.split(":")[0]);
+            let end_time = parseInt(schedule.end_time.split(":")[0]);
+            let total_time = end_time - start_time;
+
+            let total_booked = await Consultation.query()
+                .where(Engine.id("mentor"), mentor.id)
+                .where('date', date)
+                .where('approval_status', '<>', 1)
+                .getCount();
+
+            available.push({
+                date: date,
+                total_schedule: total_time,
+                available: total_time - total_booked,
+                booked: total_booked
+            })
         }
 
-        return response.success(await Specialization.createMany(payloads))
+        return response.success(available)
+    }
+
+    /**
+     * get available mentor by given date
+     *
+     * @method available
+     * @async
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<void|*>}
+     */
+    async available({request, response}) {
+        const date = request.input("date")
+        const day = new Date(date).getDay() + 1;
+        const available = [];
+
+        const mentors = (await User.query()
+            .where('role_id', 2)
+            .fetch()).toJSON();
+
+        for (let x of mentors) {
+            try {
+                let schedule = (await Schedule.query()
+                    .where("day", day)
+                    .where(Engine.id("mentor"), x.id)
+                    .fetch()).toJSON()[0];
+
+                let total = await Consultation.query()
+                    .where('date', date)
+                    .where(Engine.id("mentor"), x.id)
+                    .where('approval_status', '<>', 2)
+                    .getCount();
+
+                let aggregator = await AggregatorMentor.findBy(Engine.id("mentor"), x.id);
+                if (aggregator != null) {
+
+                    let start_time = parseInt(schedule.start_time.split(":")[0]);
+                    let end_time = parseInt(schedule.end_time.split(":")[0]);
+                    let time = end_time - start_time;
+
+                    x.total_schedule = time;
+                    x.available = time - total;
+                    x.aggregator_profile = await aggregator.aggregatorProfile().fetch();
+
+                    available.push(x)
+                }
+            } catch (e) {
+            }
+        }
+
+        return response.success(available)
+    }
+
+    /**
+     * show mentor schedule
+     *
+     * @method show
+     * @async
+     *
+     * @param response
+     * @param params
+     * @returns {Promise<void|*>}
+     */
+    async show({response, params}) {
+        const schedule = await Schedule.query().where(Engine.id("mentor"), params.id).fetch();
+        return response.success(schedule)
+    }
+
+    /**
+     * update mentor schedule
+     *
+     * @method update
+     * @async
+     *
+     * @param params
+     * @param request
+     * @param response
+     * @returns {Promise<void|*|{data: *, message: string, status: boolean}>}
+     */
+    async update({params, request, response}) {
+        const schedule = await Schedule.find(params.id);
+        if (schedule == null) return response.notFound("Schedule");
+        schedule.merge(request.all());
+        await schedule.save();
+        return response.success(schedule)
+    }
+
+    /**
+     * delete reader schedule
+     *
+     * @method destroy
+     * @async
+     *
+     * @param params
+     * @param response
+     * @returns {Promise<void|*>}
+     */
+    async destroy({params, response}) {
+        const schedule = await Schedule.find(params.id);
+        if (schedule != null) await schedule.delete();
+        return response.success(null)
     }
 }
 
-module.exports = MentorController;
+module.exports = MentorScheduleController;
